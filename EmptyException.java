ff8a3da2728183e0d077b2937ba4489aec20f2e4
Radhesk2000 <112677022+Radhesk2000@users.noreply.github.com>
2022-10-20 20:19:53 +0530

    Add files via upload

---
+++ EmptyException.java
@@ -0,0 +1,10 @@
+package com.excetion.test;
+
+public class EmptyException extends Exception{
+
+
+	public EmptyException(String msg) {
+		super(msg);
+	}
+	
+}
---
+++ NullException.java
@@ -0,0 +1,9 @@
+package com.excetion.test;
+
+
+public class NullException extends Exception {
+
+	public NullException(String msg) {
+		super(msg);
+	}
+}
